2cb3da5993adcfb730b3ed3ff54e4f39e07b6ddf
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2022-06-07 11:17:14 +0300

    Fix missing import in the webcomponent

--- enhanced-dialog/src/main/java/com/vaadin/componentfactory/EnhancedDialog.java
+++ enhanced-dialog/src/main/java/com/vaadin/componentfactory/EnhancedDialog.java
@@ -41,7 +41,7 @@ import com.vaadin.flow.dom.Element;
  * @author Vaadin Ltd
  */
 @Tag("vcf-enhanced-dialog")
-@NpmPackage(value = "@vaadin-component-factory/vcf-enhanced-dialog", version = "23.0.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-enhanced-dialog", version = "23.0.2")
 @JsModule("@vaadin-component-factory/vcf-enhanced-dialog")
 @SuppressWarnings("serial")
 public class EnhancedDialog extends Dialog {
